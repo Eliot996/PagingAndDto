da58395f16a73ffc335cb6dbe28d5dc3948a94ca
Eliot996 <math10t6@stud.kea.dk>
2022-09-26 15:01:13 +0200

    moved DTO mapper to controller from service

--- src/main/java/com/example/paginganddto/employee/service/EmployeeService.java
+++ src/main/java/com/example/paginganddto/employee/service/EmployeeService.java
@@ -1,9 +1,7 @@
 package com.example.paginganddto.employee.service;
 
-import com.example.paginganddto.employee.dto.EmployeeDto;
 import com.example.paginganddto.employee.model.Employee;
 import com.example.paginganddto.employee.repo.EmployeeRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService
@@ -21,24 +18,16 @@ public class EmployeeService
     @Autowired
     EmployeeRepository repository;
 
-    ModelMapper mapper = new ModelMapper();
-
-    public List<EmployeeDto> getAllEmployees(Integer pageNo, Integer pageSize, String sortBy)
+    public List<Employee> getAllEmployees(Integer pageNo, Integer pageSize, String sortBy)
     {
         Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
 
         Page<Employee> pagedResult = repository.findAll(paging);
 
-
-
-
         if(pagedResult.hasContent()) {
-            return pagedResult.getContent()
-                    .stream()
-                    .map(employee -> mapper.map(employee, EmployeeDto.class))
-                    .collect(Collectors.toList());
+            return pagedResult.getContent();
         } else {
-            return new ArrayList<EmployeeDto>();
+            return new ArrayList<Employee>();
         }
     }
 }
