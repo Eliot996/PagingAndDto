1c1e6e3e2b874f55bfd262fdaa0b02371efd3353
Eliot996 <math10t6@stud.kea.dk>
2022-09-26 13:59:51 +0200

    added logging and custom h2 path

--- src/main/java/com/example/paginganddto/PagingAndDtoApplication.java
+++ src/main/java/com/example/paginganddto/PagingAndDtoApplication.java
@@ -2,17 +2,19 @@ package com.example.paginganddto;
 
 import com.example.paginganddto.employee.model.Employee;
 import com.example.paginganddto.employee.repo.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @SpringBootApplication
 public class PagingAndDtoApplication {
+    private static final Logger log = LoggerFactory.getLogger(PagingAndDtoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(PagingAndDtoApplication.class, args);
@@ -23,7 +25,7 @@ public class PagingAndDtoApplication {
         return (args) -> {
 
             /**
-             *  Orders
+             *  Employee
              */
             final List<Employee> orders = new ArrayList<>();
             orders.add(new Employee("Cam", "Vi", "CVI@mail.com"));
@@ -33,8 +35,9 @@ public class PagingAndDtoApplication {
             orders.add(new Employee("Mo", "Fukerwolff", "MFukerwolff@mail.com"));
 
             repo.saveAll(orders);
-        };
 
+            log.info("Application ready");
+        };
     }
 
 }
