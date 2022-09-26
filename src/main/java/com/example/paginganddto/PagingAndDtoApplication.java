f5b73b8e681e6648f7c76b1e446e8824bb794b3a
Eliot996 <math10t6@stud.kea.dk>
2022-09-26 12:52:46 +0200

    init

---
+++ src/main/java/com/example/paginganddto/PagingAndDtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.paginganddto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagingAndDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagingAndDtoApplication.class, args);
+    }
+
+}
