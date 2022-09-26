18baa030189b983f983f5e5ac4a37b24e6e7728c
Eliot996 <math10t6@stud.kea.dk>
2022-09-26 15:01:13 +0200

    moved DTO mapper to controller from service

--- src/main/java/com/example/paginganddto/employee/controller/EmployeeController.java
+++ src/main/java/com/example/paginganddto/employee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.paginganddto.employee.controller;
 import com.example.paginganddto.employee.dto.EmployeeDto;
 import com.example.paginganddto.employee.model.Employee;
 import com.example.paginganddto.employee.service.EmployeeService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -21,14 +23,20 @@ public class EmployeeController
     @Autowired
     EmployeeService service;
 
+    ModelMapper mapper = new ModelMapper();
+
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(
             @RequestParam(defaultValue = "0") Integer pageNo,
             @RequestParam(defaultValue = "10") Integer pageSize,
             @RequestParam(defaultValue = "id") String sortBy)
     {
-        List<EmployeeDto> list = service.getAllEmployees(pageNo, pageSize, sortBy);
+        if (pageSize <= 0) return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        if (pageNo < 0) return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.BAD_REQUEST);
 
+        List<EmployeeDto> list = service.getAllEmployees(pageNo, pageSize, sortBy).stream()
+                .map(employee -> mapper.map(employee, EmployeeDto.class))
+                .collect(Collectors.toList());
         return new ResponseEntity<List<EmployeeDto>>(list, new HttpHeaders(), HttpStatus.OK);
     }
 }
